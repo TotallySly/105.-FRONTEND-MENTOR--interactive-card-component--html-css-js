9a6cc4e087f5d74fc07567e67f458524e9a32c84
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-04-24 10:30:00 +0200

    Solved issue and displaying dynamic rating

--- script.js
+++ script.js
@@ -3,16 +3,15 @@
 const submitBtn = document.querySelector('.btn--submit')
 const hideContainer = document.querySelector('.vanish')
 const showContainer = document.querySelector('.invisible')
-const ratings = document.querySelectorAll('.btn')
-const showRating = document.querySelector('.show--rating')
+const formRating = document.querySelector('form')
+let rating = document.querySelectorAll('input')
+let showValue = document.querySelector('.show--rating')
 
-ratings.forEach((rating) => {
-    rating.addEventListener('click', (e) => {
-        showRating.innerHTML = rating.innerHTML
-        rating.classList.toggle('active')
+for (let i = 0; i < rating.length; i++) {
+    rating[i].addEventListener('click', function () {
+        showValue.innerHTML = rating[i].value
     })
-})
-
+}
 submitBtn.addEventListener('click', () => {
     hideContainer.classList.add('display--none')
     showContainer.classList.remove('display--none')
